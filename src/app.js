bab37f9820e3006e3aabbf066f7a9e49e8e3e288
Segev Gershon <segev.gersh@gmail.com>
2021-07-03 19:36:15 +0300

    Fixed app main page

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import '~/styles/columns.css';
+
+function App()
+{
+    return(
+        <div>
+            <div className="col-3"> </div>
+            <div className="col-6">
+            </div>
+            <div className="col-3"> </div>
+        </div>
+    );
+};
+
+export default App;
