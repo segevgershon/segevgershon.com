74142453498eb1de212f4738dc64f07fa05d8564
Segev Gershon <segev.gersh@gmail.com>
2021-09-22 18:50:10 +0300

    updated links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,16 +12,16 @@ function Footer() {
 
             <Col md={4} sm={6} xs={12}>
                 <ul className="social-icons">
-                    <li><a className="github" href="#">
+                    <li><a className="github" href="https://github.com/GaintDwarf">
                         <FontAwesomeIcon icon={faGithub}/>
                     </a></li>
-                    <li><a className="instagram" href="#">
+                    <li><a className="instagram" href="https://www.instagram.com/segevgershon/">
                         <FontAwesomeIcon icon={faInstagram}/>
                     </a></li>
-                    <li><a className="spotify" href="#">
+                    <li><a className="spotify" href="https://open.spotify.com/user/5wvconvwzeyxa0vb997ginzkv?si=437288757c3843e8">
                         <FontAwesomeIcon icon={faSpotify}/>
                     </a></li>
-                    <li><a className="linkedin" href="#">
+                    <li><a className="linkedin" href="https://www.linkedin.com/in/segev-gershon-068299204/">
                         <FontAwesomeIcon icon={faLinkedinIn}/>
                     </a></li>
                 </ul>
